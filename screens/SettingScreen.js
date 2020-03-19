d8080d59d25df35c0f5f9b87f9d80e5ad979ceff
vishalgaddam873 <vishal18@navgurukul.org>
2020-03-19 11:12:41 +0530

    Stage 4

---
+++ screens/SettingScreen.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+export default class SettingScreen extends Component{
+    render(){
+      return(
+        <View style={styles.container}>
+          <Text>Settings screen</Text>
+        </View>
+      )
+    }
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center'
+  }
+})
